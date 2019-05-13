4248c055c6c65fa57f07ec07466456ae33a3e89e
Rio Arjuna <ri0arjuna@programmer.net>
2019-05-13 22:30:50 +0700

    first commit

---
+++ app/src/main/java/arjuna/ri0/programmerbook/MainActivity.java
@@ -0,0 +1,13 @@
+package arjuna.ri0.programmerbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
